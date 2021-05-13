c69d93f4b3856b0fc223397fdd53891680ccd858
박병찬 <66015002+qkrqudcks7@users.noreply.github.com>
2021-05-13 18:24:08 +0900

    21/5/13

--- 최단경로/전보.py
+++ 최단경로/전보.py
@@ -1,17 +1,16 @@
-import sys
 import heapq
+import sys
 
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m = map(int, input().split())
-start = int(input())
+n, m, start = map(int, input().split())
 graph = [[] for i in range(n + 1)]
 distance = [INF] * (n + 1)
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+for i in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
 
 
 def dijkstra(start):
@@ -23,10 +22,8 @@ def dijkstra(start):
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
             continue
-
         for i in graph[now]:
             cost = dist + i[1]
-
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
@@ -34,8 +31,10 @@ def dijkstra(start):
 
 dijkstra(start)
 
-for i in range(1, n + 1):
-    if distance[i] == INF:
-        print("X")
-    else:
-        print(distance[i])
+count = 0
+max_distance = 0
+for i in distance:
+    if i != INF:
+        count += 1
+        max_distance = max(max_distance, i)
+print(count - 1, max_distance)
