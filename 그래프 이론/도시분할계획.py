8b048b006f99ac44854b50bf57bec986fd4964c2
박병찬 <66015002+qkrqudcks7@users.noreply.github.com>
2021-06-16 14:29:21 +0900

    21/6/16

--- 그래프 이론/도시분할계획.py
+++ 그래프 이론/도시분할계획.py
@@ -5,8 +5,8 @@ def find_parent(parent, x):
 
 
 def union_parent(parent, a, b):
-    a = find_parent(a)
-    b = find_parent(b)
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
 
     if a < b:
         parent[b] = a
@@ -17,7 +17,9 @@ def union_parent(parent, a, b):
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
 
+# 모든 간선을 담을 리스트
 edges = []
+# 최종 비용을 담을 변수
 result = 0
 
 for i in range(1, v + 1):
@@ -27,14 +29,18 @@ for i in range(e):
     a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
 
+# 간선을 비용순으로 정렬
 edges.sort()
-# 최소 신장 트리에 포함되는 간선 중에서 가장 비용이 큰 간선
+# 최소 산장 트리에 포함되는 간성 중 가장 비용이 큰 간선
+# 크루스칼로 모두 이어져 있으니 이걸 빼면 두개로 나뉘어짐
 last = 0
 
 for i in edges:
     cost, a, b = i
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
         result += cost
+        # 비용순으로 했으니 for문 맨 마지막이 cost
         last = cost
 print(result - last)
--- 그래프 이론/커리큘럼.py
+++ 그래프 이론/커리큘럼.py
@@ -1,11 +1,41 @@
 from collections import deque
 import copy
 
-v = int(input())
-indegree = [0] * (v + 1)
-graph = [[] for i in range(v + 1)]
-time = [0] * (v + 1)
+v=int(input())
+# 모든 노드에 대한 진입차수는 0으로 초기화
+indegree = [0]*(v+1)
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
+graph =[[] for i in range(v+1)]
+# 각 강의 시간을 0으로 초기화
+time =[0]*(v+1)
 
-for i in range(1, v + 1):
-    data = list(map(int, input().split()))
-    time[i] = data[0]
+# 방향 그래프의 모든 간선 입력
+for i in range(1,v+1):
+    data = list(map(int,input().split()))
+    # 첫번째는 시간 정보를 담고 있으므로 time에 저장
+    time[i]=data[0]
+    for j in data[1:-1]:
+        indegree[i]+=1
+        graph[j].append(i)
+
+def topology_sort():
+    result = copy.deepcopy(time)
+    q=deque()
+
+    # 진입차수가 0인 노드를 큐에 삽입
+    for i in range(1,v+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now=q.popleft()
+        # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
+        for i in graph[now]:
+            result[i] = max(result[i],result[now]+time[i])
+            indegree[i]-=1
+            if indegree[i]==0:
+                q.append(i)
+    for i in range(1,v+1):
+        print(result[i])
+topology_sort()
+
