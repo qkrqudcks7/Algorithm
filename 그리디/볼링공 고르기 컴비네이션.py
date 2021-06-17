a138bc2eb3b61cc98b02b8a3defba2ff77ea6eb5
박병찬 <66015002+qkrqudcks7@users.noreply.github.com>
2021-06-17 12:11:00 +0900

    21/6/17

--- 그리디/볼링공 고르기 컴비네이션.py
+++ 그리디/볼링공 고르기 컴비네이션.py
@@ -1,11 +1,11 @@
 from itertools import combinations
 
-n,m=map(int,input().split())
-weight = list(map(int,input().split()))
+n, m = map(int, input().split())
+weight = list(map(int, input().split()))
 
-result=0
+result = 0
 
-for a,b in combinations(weight,2):
-    if a!=b:
-        result+=1
+for a, b in combinations(weight, 2):
+    if a != b:
+        result += 1
 print(result)
