dabde3f1ac69c9918b93355287daac3807758929
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-07-22 23:27:50 +0900

    7/22

--- 연습장.py
+++ 연습장.py
@@ -1,14 +1,1 @@
-def solution(n):
-    answer = 0
-    temp=0
-    for i in range(1,n+1):
-        temp+=i
-        for j in range(i+1,n+1):
-            if temp==n:
-                answer+=1
-                temp=0
-                break
-            else:
-                temp+=j 
-    return answer
-print(solution(15))
+aaaa
