6b869c65db935319a7bdf6a8983dcb0b528b1d26
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-07-25 16:35:18 +0900

    7/25

--- 구현/자물쇠와 열쇠.py
+++ 구현/자물쇠와 열쇠.py
@@ -1,39 +1,48 @@
-def rotate(key):
-    return list(zip(*key[::-1]))
+def attach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x + i][y + j] += key[i][j]
 
 
-# 자물쇠의 중간 부분이 모두 1인지 확인
-def check(new_lock):
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length * 2):
-        for j in range(lock_length, lock_length * 2):
-            if new_lock[i][j] != 1:
-                return False
+def detach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x + i][y + j] -= key[i][j]
+
+
+def rotate90(arr):
+    return list(zip(*arr[::-1]))
+
+
+def check(board, M, N):
+    for i in range(N):
+        for j in range(N):
+            if board[M + i][M + j] != 1:
+                return False;
     return True
 
 
 def solution(key, lock):
-    n = len(lock)
-    m = len(key)
+    M, N = len(key), len(lock)
 
-    new_lock = [[0] * (n * 3) for i in range(n * 3)]
+    board = [[0] * (M * 2 + N) for _ in range(M * 2 + N)]
+    # 자물쇠 중앙 배치
+    for i in range(N):
+        for j in range(N):
+            board[M + i][M + j] = lock[i][j]
 
-    #  새로운 자물쇠의 중앙에 기존 자물쇠 넣기
-    for i in range(n):
-        for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
-
-    for rotation in range(4):
-        key = rotate(key)
-        for x in range(n * 2):
-            for y in range(n * 2):
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-                if check(new_lock):
+    rotated_key = key
+    # 모든 방향 (4번 루프)
+    for _ in range(4):
+        rotated_key = rotate90(rotated_key)
+        for x in range(1, M + N):
+            for y in range(1, M + N):
+                # 열쇠 넣어보기
+                attach(x, y, M, rotated_key, board)
+                # lock 가능 check
+                if (check(board, M, N)):
                     return True
+                # 열쇠 빼기
+                detach(x, y, M, rotated_key, board)
 
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + i] -= key[i][j]
     return False
