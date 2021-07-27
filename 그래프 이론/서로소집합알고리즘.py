8407946862abbdcbd1dc67e899da394523e6cd7c
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-07-27 22:56:04 +0900

    7/27

--- 그래프 이론/서로소집합알고리즘.py
+++ 그래프 이론/서로소집합알고리즘.py
@@ -8,22 +8,14 @@ def find_parent(parent, x):
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+    a, b = find_parent(parent, a), find_parent(parent, b)
+    parent[max(a, b)] = min(a, b)
 
 
 # 노드의 개수와 간선의 개수 입력받기
 v, e = map(int, input().split())
-# 부모테이블 초기화
-parent = [0] * (v + 1)
-
 # 부모테이블에서, 부모를 자기 자신으로 초기화
-for i in range(1, v + 1):
-    parent[i] = i
+parent = [i for i in range(v + 1)]
 
 # union 연산을 각각 수행
 for i in range(e):
--- 그래프 이론/크루스칼알고리즘.py
+++ 그래프 이론/크루스칼알고리즘.py
@@ -5,22 +5,15 @@ def find_parent(parent, x):
 
 
 def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+    a, b = find_parent(parent, a), find_parent(parent, b)
+    parent[max(a, b)] = min(a, b)
 
 
 v, e = map(int, input().split())
-parent = [0] * (v + 1)
+parent = [i for i in range(v + 1)]
 
 edges = []
 result = 0
-
-for i in range(1, v + 1):
-    parent[i] = i
 
 for i in range(e):
     a, b, cost = map(int, input().split())
