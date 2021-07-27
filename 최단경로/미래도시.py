ed1b1475b8a3c0f432075659febe94519520b1a4
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-07-28 00:14:04 +0900

    7/27

--- 최단경로/미래도시.py
+++ 최단경로/미래도시.py
@@ -3,10 +3,8 @@ INF = int(1e9)
 n, m = map(int, input().split())
 graph = [[INF] * (n + 1) for i in range(n + 1)]
 
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if a == b:
-            graph[a][b] = 0
+for i in range(1,n+1):
+    graph[i][i] = 0
 
 for i in range(m):
     # a와b가 서로에게 가는 비용은 1
