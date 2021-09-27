c7cf9a06204be3241e193b2ecd153bd40d7cd540
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-27 15:44:59 +0900

    백트랙킹

--- 백트랙킹/N-Queen.py
+++ 백트랙킹/N-Queen.py
@@ -1,14 +1,8 @@
-# 한 열에 하나의 퀸
-# 한 행에 하나의 퀸
-# 한 대각선에 하나의 퀸만 존재해야 된다.
-# i번째 행에 j라는 요소를 넣어 1차원 배열로 표현
-
 n = int(input())
 count = 0
 board = [-1 for i in range(n)]
 
 
-# i행에 j번째에 퀸을 배치할 수 있는가
 def is_possible(i, j):
     for x in range(i):
         if j == board[x] or (abs(i - x) == abs(j - board[x])):
