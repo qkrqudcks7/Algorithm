4cf2ce7c151cc874e03e689a353cf00f082fe131
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-10-03 01:33:22 +0900

    dp

--- DP/가장 긴 증가하는 부분 수열(LIS).py
+++ DP/가장 긴 증가하는 부분 수열(LIS).py
@@ -3,7 +3,7 @@ n = int(input())
 s = list(map(int, input().split()))
 
 dp = [1] * n
-for i in range(1, n):
+for i in range(n):
     for j in range(i):
         # 길이를 찾으려는 숫자가 비교할 숫자보다 크면, 그 숫자가 가지고 있는 길이+1과 자신이 길이를 비교해서 큰 값으로 최신화 한다.
         if s[i] > s[j]:
