b6d4b56066dd71f29e4e6fbcb9244811743a32bd
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-10-17 00:30:41 +0900

    평범한 배낭

--- DP/평범한 배낭(암기).py
+++ DP/평범한 배낭(암기).py
@@ -6,7 +6,7 @@ for i in range(n):
     w, v = map(int, input().split())
     stuff.append([w, v])
 
-bag = [[0 for i in range(k + 1)] for i in range(n + 1)]
+bag = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, k + 1):
