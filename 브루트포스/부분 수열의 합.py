c3a63d7c1bf7bf023d54d56696634e5baea4d0e2
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-10-25 23:20:39 +0900

    컴비네이션

--- 브루트포스/부분 수열의 합.py
+++ 브루트포스/부분 수열의 합.py
@@ -1,17 +1,13 @@
-def dfs(idx, sum):
-    global count
-    if idx >= n:
-        return
-    sum += arr[idx]
-    if sum == s:
-        count += 1
-    dfs(idx + 1, sum - arr[idx])
-    dfs(idx + 1, sum)
-
+from itertools import combinations
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 count = 0
 
-dfs(0, 0)
+for i in range(1, n + 1):
+    temp = combinations(arr, i)
+    for j in temp:
+        if sum(j) == s:
+            count += 1
+
 print(count)
