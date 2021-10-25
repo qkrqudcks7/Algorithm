c9cc8bb123870daaa19d4f486e907bba98dbba33
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-10-25 20:29:20 +0900

    부루트 포스

--- 연습장.py
+++ 연습장.py
@@ -1,33 +1,5 @@
-board = [list(map(int, input().split())) for i in range(9)]
-zero = [(i, j) for j in range(9) for i in range(9) if board[i][j] == 0]
+a=5
 
-
-def solve(i):
-    if i == len(zero):
-        [print(*row) for row in board]
-
-    r, c = zero[i]
-
-    # 행
-    possible = set(range(1, 10))
-    possible -= set(board[r])
-
-    # 열
-    test = set()
-    for i in range(9):
-        test.add(board[i][c])
-    possible -= test
-
-    test = set()
-    for i in range(r // 3 * 3, r // 3 * 3 + 3):
-        for j in range(c // 3 * 3, c // 3 * 3 + 3):
-            test.add(board[i][j])
-    possible -= test
-
-    for i in tuple(possible):
-        board[r][c] = i
-        solve(i + 1)
-        board[r][c] = 0
-
-
-solve(0)
+if a==5:
+    break
+    
