559ad212108eb71cd5ed4fe09b4b399b9bfa0935
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-11-05 23:46:25 +0900

    bfs

---
+++ DFS_BFS/물통.py
@@ -0,0 +1,5 @@
+from collections import deque
+
+a, b, c = map(int, input().split())
+
+check = [[0] * 201 for i in range(201)]
--- 연습장.py
+++ 연습장.py
@@ -1,5 +1,28 @@
-a=5
+# 071203-456789
 
-if a==5:
-    break
-    
+while True:
+    number = input("주민등록번호를 입력해주세요:")
+
+    if number[6] == "-":
+        break
+    else:
+        print("다시 입력해주세요!")
+
+temp = ""
+if number[0] == '0' or number[0] == '1' or number[0] == '2':
+    temp = '20' + number[0] + number[1] + "년생"
+
+    if number[7] == '3':
+        temp += " 남자"
+    elif number[7] == '4':
+        temp += " 여자"
+
+else:
+    temp = '19' + number[0] + number[1] + "년생"
+
+    if number[7] == '1':
+        temp += " 남자"
+    elif number[7] == '2':
+        temp += " 여자"
+
+print(temp)
