7595f83bd308db3c883093d50ec5ca36b6c0919b
박병찬 <byeongchan.park>
2021-11-17 19:05:32 +0900

    재귀

--- 브루트포스/퇴사.py
+++ 브루트포스/퇴사.py
@@ -1,17 +1,16 @@
-# 퇴사 DP
 n = int(input())
-T, P = [0 for i in range(n + 1)], [0 for i in range(n + 1)]
+t = []
+p = []
 for i in range(n):
     a, b = map(int, input().split())
-    T[i] = a
-    P[i] = b
+    t.append(a)
+    p.append(b)
 
-# dp[i]는 i번째날까지 일을 했을 때, 최대값이다.
-dp = [0 for i in range(n + 1)]
+dp = [0] * (n + 1)
 
-for i in range(len(T) - 2, -1, -1):  # 역순으로 진행
-    if T[i] + i <= n:  # 날짜를 초과하지 않을 경우.
-        dp[i] = max(P[i] + dp[i + T[i]], dp[i + 1])
-    else:  # 날짜를 초과할 경우.
+for i in range(n - 1, -1, -1):
+    if t[i] + i > n:
         dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(p[i] + dp[i + t[i]], dp[i + 1])
 print(dp[0])
