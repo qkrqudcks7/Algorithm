9278b2b68f5012f98c97f7131d1728da90839fdf
박병찬 <byeongchan.park>
2021-11-17 18:37:06 +0900

    재귀

--- 브루트포스/암호 만들기.py
+++ 브루트포스/암호 만들기.py
@@ -1,25 +1,24 @@
 from itertools import combinations
 
-l, c = map(int, input().split())
+a, b = map(int, input().split())
 
 arr = list(map(str, input().split()))
 arr.sort()
 answer = []
-for i in combinations(arr, l):
+for i in combinations(arr, a):
     answer.append(i)
-
 
 for i in answer:
     mC = 0
-    cC = 0
+    jC = 0
     for j in i:
         if j in ['a', 'e', 'i', 'o', 'u']:
             mC += 1
         else:
-            cC += 1
-    if mC < 1 or cC < 2:
+            jC += 1
+    if mC < 1 or jC < 2:
         pass
     else:
         print("".join(i))
     mC = 0
-    cC = 0
+    jC = 0
